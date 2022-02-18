0b851cecca222348b0a220aca2bc53e845c66200
FetaFisken00 <Cullberg.aron@gmail.com>
2022-02-18 12:41:16 +0100

    it works

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Wordle from "./Wordle";
 import { v4 as uuid } from "uuid";
 
 const LETTER_COUNT: number = 30;
-const WORD: string = "LOCKS";
+// const wordKey: string = "LOCKS";
 
 interface LETTER_LIST_INTERFACE {
     key: string;
@@ -27,59 +27,22 @@ function App() {
         setLetterList(tempList);
     }
 
+    function sanitizeInput() {
+        
+    }
+
     function handleAnswer() {
         console.log('HEOLO')
     }
 
-    function handleInput() {
-        console.log("hanldeInput is called");
+    function handleInput(e: any) {
+        console.log(e);
         const input = (inputRef.current as HTMLInputElement).value;
         const newLetterList = [...letterList];
 
         let lastLetter: number = newLetterList.length - 1;
         let wordIndexStart: number = 0;
         let wordIndexEnd: number = 0;
-
-        document.addEventListener("keydown", function (event) {
-            if (event.ctrlKey || (event.altKey && event.key === "Backspace")) {
-                console.log(event);
-                event.preventDefault();
-                // console.log("ctrl Backspace");
-                return;
-            }
-            if (event.key === "ArrowUp" || event.key === "ArrowRight" || event.key === "ArrowDown" || event.key === "ArrowLeft") {
-                console.log(event);
-                event.preventDefault();
-                return;
-            }
-            if (event.key === "Home" || event.key === "End") {
-                console.log(event);
-                event.preventDefault();
-                return;
-            }
-            if (event.key === "Enter" && input.length === 5) {
-                console.log("ENTER");
-                // TODO: MAKE ENTER KEY WORK
-                for (let i = 0; i < input.length; i++) {
-                    console.log(i);
-                    const test = newLetterList[i];
-                    if (input.charAt(i) === WORD.charAt(i)) {
-                        console.log('correct')
-                        test.state = "correct";
-                        return;
-                    }
-                    if (WORD.split("").includes(input.split("")[i])) {
-                        console.log('wrong-location')
-                        test.state = "wrong-location";
-                        return;
-                    }
-                    console.log('wrong')
-                    test.state = "wrong";
-                    setLetterList(newLetterList)
-                }
-                return;
-            }
-        });
 
         if (!(input.charAt(input.length - 1).toUpperCase() !== input.charAt(input.length - 1).toLowerCase()) && input.length !== 0) {
             (inputRef.current as HTMLInputElement).value = input.slice(0, input.length - 1);
@@ -120,10 +83,51 @@ function App() {
 
     return (
         <>
-            <input id="wordleGuessInputBox" ref={inputRef} onChange={handleInput} onSubmit={handleAnswer} type="text" />
+            <input id="wordleGuessInputBox" ref={inputRef} onChange={handleInput} onKeyPress={(e) => e.key === "Enter" && handleAnswer()} type="text" />
             <Wordle letterList={letterList} />
         </>
     );
 }
 
 export default App;
+
+ // document.addEventListener("keydown", function (event) {
+        //     if (event.ctrlKey || (event.altKey && event.key === "Backspace")) {
+        //         console.log(event);
+        //         event.preventDefault();
+        //         // console.log("ctrl Backspace");
+        //         return;
+        //     }
+        //     if (event.key === "ArrowUp" || event.key === "ArrowRight" || event.key === "ArrowDown" || event.key === "ArrowLeft") {
+        //         console.log(event);
+        //         event.preventDefault();
+        //         return;
+        //     }
+        //     if (event.key === "Home" || event.key === "End") {
+        //         console.log(event);
+        //         event.preventDefault();
+        //         return;
+        //     }
+        //     if (event.key === "Enter" && input.length === 5) {
+        //         console.log("ENTER");
+        //         // TODO: MAKE ENTER KEY WORK
+        //         for (let i = 0; i < input.length; i++) {
+        //             console.log(i);
+        //             const test = newLetterList[i];
+        //             if (input.charAt(i) === WORD.charAt(i)) {
+        //                 console.log('correct')
+        //                 test.state = "correct";
+        //                 return;
+        //             }
+        //             if (WORD.split("").includes(input.split("")[i])) {
+        //                 console.log('wrong-location')
+        //                 test.state = "wrong-location";
+        //                 return;
+        //             }
+        //             console.log('wrong')
+        //             test.state = "wrong";
+        //             setLetterList(newLetterList)
+        //         }
+        //         return;
+        //     }
+        // });
